0511fd96f2ed4b08005161af9560f1a2f17ae9ad
Christopher <christiller2015@gmail.com>
2020-04-17 20:02:25 -0400

    Take code from github and call it a day.

--- nodes_edges_faces/wireframe.py
+++ nodes_edges_faces/wireframe.py
@@ -1,107 +1,51 @@
 import numpy as np
-import math
 
 class Wireframe:
-    """
-    A wireframe is a 3d object only composed of nodes and edges.
-    Wireframes do not possess any faces.
-    """
     def __init__(self):
         self.nodes = np.zeros((0, 4))
         self.edges = []
 
-    """
-    Adds a list of nodes to the existing set of nodes.
-    """
-    def add_nodes(self, node_array):
+    def addNodes(self, node_array):
         ones_column = np.ones((len(node_array), 1))
         ones_added = np.hstack((node_array, ones_column))
         self.nodes = np.vstack((self.nodes, ones_added))
 
-    """
-    Adds a list of edges to the existing set of edges.
-    """
-    def add_edges(self, edge_list):
-        self.edges += edge_list
+    def addEdges(self, edgeList):
+        self.edges += edgeList
 
-    """Outputs all of the values for every existing node."""
-    def output_nodes(self):
-        print("\n --- Nodes --- ")
-        for i, (x, y, z, _) in enumerate(self.nodes):
-            print(f"{i}: ({x}, {y}, {z})")
+    def outputNodes(self):
+        for i in range(self.nodes.shape[1]):
+            (x, y, z, _) = self.nodes[:, i]
+            print("Node %d: (%.3f, %.3f, %.3f)" % (i, x, y, z))
 
-    """Outputs all of the edges for every existing edge set."""
-    def output_edges(self):
-        print("\n --- Edges --- ")
-        for i, (node1, node2) in enumerate(self.edges):
-            print(f"   {i}: {node1} -> {node2}")
-    
-    def translate(self, axis, d):
-        """ Translate each node of a wireframee by d along a given axis. """
+    def outputEdges(self):
+        for i, (start, stop) in enumerate(self.edges):
+            node1 = self.nodes[:, start]
+            node2 = self.nodes[:, stop]
+            print( "Edge %d: (%.3f, %.3f, %.3f)" % (i, node1[0], node1[1], node1[2])),
+            print( "to (%.3f, %.3f, %.3f)" % (node2[0], node2[1], node2[2]))
 
-        if axis in ['x', 'y', 'z']:
-            for node in self.nodes:
-                setattr(node, axis, getattr(node, axis) + d)
-    
-    def scale(self,  centers, scale):
-        """ Scale the wireframe from the center of the screen. """
+    def findCentre(self):
+        #num_nodes = len(self.nodes)
+        #mean = np.mean(self.nodes) / num_nodes
+        mean = self.nodes.mean(axis=0)     # to take the mean of each col
+        print( mean)
+        #print "next"
+        return mean
 
-        for node in self.nodes:
-            node.x = centers[0] + scale * (node.x - centers[0])
-            node.y = centers[1] + scale * (node.y - centers[1])
-            node.z *= scale
+    def transform(self, matrix):
+        """ Apply a transformation defined by a given matrix. """
+        self.nodes = np.dot(self.nodes, matrix)
 
-    def find_center(self):
-        """
-        Find the center of the wireframe.
-        """
-        
-        num_nodes = len(self.nodes)
-        mean_x = sum([node.x for node in self.nodes]) / num_nodes
-        mean_y = sum([node.y for node in self.nodes]) / num_nodes
-        mean_z = sum([node.z for node in self.nodes]) / num_nodes
+    def scale(self, center, matrix):
+        """ Scale the wireframe from the centre of the screen """
+        for i,node in enumerate(self.nodes):
+            self.nodes[i] = center + np.matmul(matrix, node-center)
 
-        return (mean_x, mean_y, mean_z)
-
-    def rotate_z(self, centers, radians):
-        for node in self.nodes:
-            cx, cy, cz = centers
-            x = node.x - cx
-            y = node.y - cy
-            d = math.hypot(y, x)
-            theta = math.atan2(y, x) + radians
-            node.x = cx + d * np.cos(theta)
-            node.y = cy + d * np.sin(theta)
-
-    def rotate_x(self, centers, radians):
-        for node in self.nodes:
-            cx, cy, cz = centers
-            y = node.y - cy
-            z = node.z - cz
-            d = math.hypot(y, z)
-            theta = math.atan2(y, z) + radians
-            node.z = cz + d * np.cos(theta)
-            node.y = cy + d * np.sin(theta)
-
-    def rotate_y(self, centers, radians):
-        for node in self.nodes:
-            cx, cy, cz = centers
-            x = node.x - cx
-            z = node.z - cz
-            d = math.hypot(x, z)
-            theta = math.atan2(x, z) + radians
-            node.z = cz + d * np.cos(theta)
-            node.x = cx + d * np.sin(theta)
+    def rotate(self, center, matrix):
+        for i, node in enumerate(self.nodes):
+            self.nodes[i] = center + np.matmul(matrix, node-center)
 
 
-if __name__ == "__main__":
-    """With the current setup, we can add three nodes and
-    create an edge between the second two like so:"""
-    cube = Wireframe()
-    cube_nodes = [(x, y, z) for x in (0, 1) for y in (0, 1) for z in (0, 1)]
-    cube.add_nodes(np.array(cube_nodes))
-    cube.add_edges([(n, n+4) for n in range(0, 4)])
-    cube.add_edges([(n, n+1) for n in range(0, 8, 2)])
-    cube.add_edges([(n, n+2) for n in (0, 1, 4, 5)])
-    cube.output_nodes()
-    cube.output_edges()
+
+
--- projecting_3d_objects/wireframe_display.py
+++ projecting_3d_objects/wireframe_display.py
@@ -1,6 +1,22 @@
-import numpy as np
+import nodes_edges_faces.wireframe as wf
 import pygame
-from nodes_edges_faces import wireframe
+import numpy as np
+
+key_to_function = {
+
+    pygame.K_LEFT: (lambda x: x.translateAll([-10, 0, 0])),
+    pygame.K_RIGHT:(lambda x: x.translateAll([ 10, 0, 0])),
+    pygame.K_DOWN: (lambda x: x.translateAll([0,  10, 0])),
+    pygame.K_UP:   (lambda x: x.translateAll([0, -10, 0])),
+    pygame.K_EQUALS: (lambda x: x.scaleAll(1.05)),
+    pygame.K_MINUS:  (lambda x: x.scaleAll( 0.95)),
+    pygame.K_q:      (lambda x: x.rotateAll('X',  0.1)),
+    pygame.K_w:      (lambda x: x.rotateAll('X', -0.1)),
+    pygame.K_a:      (lambda x: x.rotateAll('Y',  0.1)),
+    pygame.K_s:      (lambda x: x.rotateAll('Y', -0.1)),
+    pygame.K_z:      (lambda x: x.rotateAll('Z',  0.1)),
+    pygame.K_x:      (lambda x: x.rotateAll('Z', -0.1))}
+
 
 class ProjectionViewer:
     """ Displays 3D objects on a Pygame screen """
@@ -10,35 +26,35 @@ class ProjectionViewer:
         self.height = height
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption('Wireframe Display')
-        self.background = (10, 10, 50)
+        pygame.key.set_repeat(1,10)
+        self.background = (10,10,50)
 
         self.wireframes = {}
-        self.display_nodes = True
-        self.display_edges = True
-        self.node_color = (255, 255, 255)
-        self.edge_color = (200, 200, 200)
-        self.node_radius = 4
-    
+        self.displayNodes = True
+        self.displayEdges = True
+        self.nodeColour = (255,255,255)
+        self.edgeColour = (200,200,200)
+        self.nodeRadius = 4
+
+    def addWireframe(self, name, wireframe):
+        """ Add a named wireframe object. """
+
+        self.wireframes[name] = wireframe
+
     def run(self):
-        """ Create a pygame screen until it is closed."""
+        """ Create a pygame screen until it is closed. """
 
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-
                 elif event.type == pygame.KEYDOWN:
                     if event.key in key_to_function:
                         key_to_function[event.key](self)
 
             self.display()
             pygame.display.flip()
-   
-
-    def addWireFrame(self, name, wireframe):
-        """Add a named wireframe object."""
-        self.wireframes[name] = wireframe
 
     def display(self):
         """ Draw the wireframes on the screen. """
@@ -46,68 +62,94 @@ class ProjectionViewer:
         self.screen.fill(self.background)
 
         for wireframe in self.wireframes.values():
-            if self.display_edges:
+            if self.displayEdges:
                 for n1, n2 in wireframe.edges:
-                    pygame.draw.aaline(self.screen, self.edge_color, wireframe.nodes[n1][:2], wireframe.nodes[n2][:2], 1)
+                    pygame.draw.aaline(self.screen, self.edgeColour, wireframe.nodes[n1][:2], wireframe.nodes[n2][:2], 1)
 
-            if self.display_nodes:
+            if self.displayNodes:
                 for node in wireframe.nodes:
-                    pygame.draw.circle(self.screen, self.node_color, (int(node[0]), int(node[1])), self.node_radius, 0)
+                    pygame.draw.circle(self.screen, self.nodeColour, (int(node[0]), int(node[1])), self.nodeRadius, 0)
 
-    def translateAll(self, axis, d):
+
+    def translationMatrix(self, dx, dy, dz):
+        """ Return matrix for translation along vector (dx, dy, dz). """
+
+        return np.array([[1,0,0,0],
+                         [0,1,0,0],
+                         [0,0,1,0],
+                         [dx,dy,dz,1]])
+
+    def scaleMatrix(self, sx=0, sy=0, sz=0):
+        """ Return matrix for scaling equally along all axes centred on the point (cx,cy,cz). """
+
+        return np.array([[sx, 0,  0,  0],
+                         [0,  sy, 0,  0],
+                         [0,  0,  sz, 0],
+                         [0,  0,  0,  1]])
+
+
+
+    def rotateXMatrix(self, radians):
+        """ Return matrix for rotating about the x-axis by 'radians' radians """
+
+        c = np.cos(radians)
+        s = np.sin(radians)
+        return np.array([[1, 0, 0, 0],
+                         [0, c,-s, 0],
+                         [0, s, c, 0],
+                         [0, 0, 0, 1]])
+    def rotateYMatrix(self, radians):
+        """ Return matrix for rotating about the y-axis by 'radians' radians """
+
+        c = np.cos(radians)
+        s = np.sin(radians)
+        return np.array([[ c, 0, s, 0],
+                         [ 0, 1, 0, 0],
+                         [-s, 0, c, 0],
+                         [ 0, 0, 0, 1]])
+    def rotateZMatrix(self, radians):
+        """ Return matrix for rotating about the z-axis by 'radians' radians """
+
+        c = np.cos(radians)
+        s = np.sin(radians)
+        return np.array([[c,-s, 0, 0],
+                         [s, c, 0, 0],
+                         [0, 0, 1, 0],
+                         [0, 0, 0, 1]])
+
+
+    def translateAll(self, vector):
         """ Translate all wireframes along a given axis by d units. """
-
+        matrix = self.translationMatrix(vector[0], vector[1], vector[2])
         for wireframe in self.wireframes.values():
-            wireframe.translate(axis, d)
+            wireframe.transform(matrix)
+
 
     def scaleAll(self, scale):
-        """ Scale all wireframes by a given scale, centered on the center of the screen. """
-
-        center_x = self.width/2
-        center_y = self.height / 2
-
-        for wireframe in self.wireframes.values():
-            wireframe.scale((center_x, center_y), scale)
-
-    def rotate_all(self, axis, theta):
-        """
-        Rotate all wireframe about their center, along a given axis by a given angle.
-        """
-
-        rotate_function = 'rotate_' + axis
+        """ Scale all wireframes by a given scale, centred on the centre of the screen. """
+        center = [self.width/2, self.height/2, 0, 0]
+        matrix = self.scaleMatrix(scale, scale, scale)
 
         for wireframe in self.wireframes.values():
-            center = wireframe.find_center()
-            getattr(wireframe, rotate_function)(center, theta)
+            wireframe.scale(center, matrix)
 
-key_to_function = {
-        pygame.K_LEFT: (lambda x: x.translateAll('x', -10)),
-        pygame.K_RIGHT: (lambda x: x.translateAll('x', 10)),
-        pygame.K_DOWN: (lambda x: x.translateAll('y', 10)),
-        pygame.K_UP: (lambda x: x.translateAll('y', -10)),
-        pygame.K_EQUALS: (lambda x: x.scaleAll(1.25)),
-        pygame.K_MINUS: (lambda x: x.scaleAll(0.8)),
+    def rotateAll(self, axis, theta):
+        """ Rotate all wireframe about their centre, along a given axis by a given angle. """
 
-        pygame.K_q: (lambda x: x.rotate_all('x', 0.1)),
-        pygame.K_w: (lambda x: x.rotate_all('x', -0.1)),
-        pygame.K_a: (lambda x: x.rotate_all('y', 0.1)),
-        pygame.K_s: (lambda x: x.rotate_all('y', -0.1)),
-        pygame.K_z: (lambda x: x.rotate_all('z', 0.1)),
-        pygame.K_x: (lambda x: x.rotate_all('z', -0.1)),
-        }
+        rotateFunction = 'rotate' + axis + 'Matrix'
+
+        for wireframe in self.wireframes.values():
+            center = wireframe.findCentre()
+            matrix = getattr(self, rotateFunction)(theta)
+            wireframe.rotate(center, matrix)
+
 if __name__ == '__main__':
-    cube = wireframe.Wireframe()
-    
-    cube.add_nodes(np.array([(x,y,z) for x in (50, 250) for y in (50, 250) for z in (50, 250)]))
-    cube.add_edges([(n, n+4) for n in range(0, 4)] + [(n, n+1) for n in range(0, 8, 2)] + [(n, n+2) for n in (0, 1, 4, 5)])
+    pv = ProjectionViewer(600, 500)
 
-    # cube.translate('x', 100)
-    # cube.translate('y', -40)
+    cube = wf.Wireframe()
+    cube_nodes = [(x,y,z) for x in (50,250) for y in (50,250) for z in (50,250)]
+    cube.addNodes(np.array(cube_nodes))
+    cube.addEdges([(n,n+4) for n in range(0,4)]+[(n,n+1) for n in range(0,8,2)]+[(n,n+2) for n in (0,1,4,5)])
 
-    # cube.scale((200, 150), .75)
-
-    # cube.rotateZ(cube.find_center(), 0.1)
-
-    pv = ProjectionViewer(400, 300)
-    pv.addWireFrame('cube', cube)
+    pv.addWireframe('cube', cube)
     pv.run()
